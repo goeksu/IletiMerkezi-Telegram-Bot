cd38427aa2b6ae8a7d5e8e598c4ac7387054175c
goksu <25721443+goeksu@users.noreply.github.com>
2023-04-26 17:15:39 +0300

    log saving

--- iletibot.py
+++ iletibot.py
@@ -1,8 +1,9 @@
 import requests
+import pickle
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
 
-TELEGRAM_BOT_TOKEN = "TG-BOT-API-KEY"
+TELEGRAM_BOT_TOKEN = "TG-TOKEN"
 
 #to go state by state
 STATE_WAIT_USERNAME = 0
@@ -13,6 +14,30 @@ STATE_WAIT_MESSAGE = 4
 STATE_WAIT_CONTACTS = 5
 
 user_states = {}
+user_credentials = {}
+logs = []
+
+# Load creds and logs from the pickle files
+try:
+    with open('credentials.pkl', 'rb') as credentials_file:
+        user_credentials = pickle.load(credentials_file)
+except FileNotFoundError:
+    pass
+
+try:
+    with open('logs.pkl', 'rb') as logs_file:
+        logs = pickle.load(logs_file)
+except FileNotFoundError:
+    pass
+
+def save_credentials():
+    with open('credentials.pkl', 'wb') as credentials_file:
+        pickle.dump(user_credentials, credentials_file)
+
+def save_logs():
+    with open('logs.pkl', 'wb') as logs_file:
+        pickle.dump(logs, logs_file)
+
 
 #The used API that includes many security vulnerability and not acceptable to law.
 def send_sms(username, password, sender_name, number, message):
@@ -46,7 +71,7 @@ def start(update: Update, context: CallbackContext):
         "/start - Start the bot\n"
         "/newmessage - Start a new message\n"
         "/forgetme - Forget the saved credentials and stop the current process\n\n"
-        "Please enter your username:"
+        "Please enter your username (05XXXXXXXXX):"
     )
 
 def new_message(update: Update, context: CallbackContext):
@@ -79,8 +104,14 @@ def handle_message(update: Update, context: CallbackContext):
     text = update.message.text
 
     if user_id not in user_states:
-        start(update, context)
-        return
+        if user_id in user_credentials:
+            user_states[user_id] = STATE_WAIT_MESSAGE
+            context.user_data.update(user_credentials[user_id])
+            update.message.reply_text("Credentials loaded. Please enter your message.")
+            return
+        else:
+            start(update, context)
+            return
 
     state = user_states[user_id]
     #decide as the message pattern
@@ -105,9 +136,14 @@ def handle_message(update: Update, context: CallbackContext):
             test_number,
             "Test"
         )
-        #test message for first time login. after that it loops to new contact or new messages
         if status_code == 200:
             user_states[user_id] = STATE_WAIT_MESSAGE
+            user_credentials[user_id] = {
+                "username": context.user_data["username"],
+                "password": context.user_data["password"],
+                "sender_name": context.user_data["sender_name"],
+            }
+            save_credentials()
             update.message.reply_text("Credentials saved. Please enter your message.")
         else:
             del user_states[user_id]
@@ -115,6 +151,7 @@ def handle_message(update: Update, context: CallbackContext):
             del context.user_data["password"]
             del context.user_data["sender_name"]
             update.message.reply_text(f"Invalid credentials. Status code: {status_code}, Reason: {reason}")
+
     elif state == STATE_WAIT_MESSAGE:
         context.user_data["message"] = text
         user_states[user_id] = STATE_WAIT_CONTACTS
@@ -136,6 +173,16 @@ def handle_contact(update: Update, context: CallbackContext):
         context.user_data["message"]
     )
 
+    log_entry = {
+        "user_id": user_id,
+        "phone_number": phone_number,
+        "message": context.user_data["message"],
+        "status_code": status_code,
+        "reason": reason
+    }
+    logs.append(log_entry)
+    save_logs()
+
     if status_code == 200:
         update.message.reply_text(f"Message sent to {phone_number}.")
     else:
@@ -156,4 +203,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
